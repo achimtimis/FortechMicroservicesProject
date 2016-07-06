8d072285a952233bf16c956393e05b15eabcaee7
Achim <achytimis@gmail.com>
2016-07-06 14:15:40 +0300

    adding minor dependency changes,and a custom query test

--- user-service/src/main/java/com/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/userservice/repository/UserRepository.java
@@ -2,9 +2,16 @@ package com.userservice.repository;
 
 import com.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 /**
  * Created by Achim Timis on 7/6/2016.
  */
 public interface UserRepository extends JpaRepository<User,Long> {
+//
+//    @RestResource(path="by-name")
+//    List<User> findByName(@Param("name") String name);
+
 }
