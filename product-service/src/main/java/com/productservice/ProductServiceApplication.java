b0ec71b7e510f36d16d6d2c3740dfcea2101c2c0
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-06 18:13:47 +0300

    Adding Zuul to the project, integrated with user-service and product-service

--- product-service/src/main/java/com/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/productservice/ProductServiceApplication.java
@@ -1,9 +1,22 @@
 package com.productservice;
 
+import com.productservice.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Type;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,4 +26,5 @@ public class ProductServiceApplication {
 
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
+
 }
