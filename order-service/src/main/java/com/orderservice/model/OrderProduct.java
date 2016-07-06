28530cf98bcb1c918494ce0869166388f1404c32
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-06 16:53:43 +0300

    Add order service

---
+++ order-service/src/main/java/com/orderservice/model/OrderProduct.java
@@ -0,0 +1,102 @@
+package com.orderservice.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+/**
+ * Created by Flaviu Cicio on 07.07.2016.
+ */
+@Entity
+@Table(name = "order_product")
+public class OrderProduct {
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="ORDER_ID")
+    private Order byOrder;
+
+    private Long productId;
+
+    private String productName;
+
+    private int productPrice;
+
+    private int quantity;
+
+    public OrderProduct() {
+    }
+
+    public OrderProduct(Long id, Order order, Long productId, String productName, int productPrice, int quantity) {
+        this.id = id;
+        this.byOrder = order;
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @JsonIgnore
+    public Order getOrder() {
+        return byOrder;
+    }
+
+    public void setOrder(Order order) {
+        this.byOrder = order;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderProduct{" +
+                "id=" + id +
+//                ", orderId=" + byOrder.getId() +
+                ", productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productPrice=" + productPrice +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
