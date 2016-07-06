4a8358498b45d45a56bb7717966e99db375cb448
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-06 17:29:21 +0300

    integrating user-service with config-server

--- product-service/src/main/java/com/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/productservice/ProductServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
