3bf12133f0b3b5280b9ac94ee0bc2699271e26d5
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-06 16:53:43 +0300

    config server

--- product-service/src/main/java/com/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/productservice/ProductServiceApplication.java
@@ -2,11 +2,14 @@ package com.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 }
