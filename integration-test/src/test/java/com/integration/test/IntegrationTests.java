ffe86ccb0a048de6c5aff66cf32ac02171333b88
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-27 11:41:19 +0300

    Product-Service integration tests restructured to be more specific on each service

--- integration-test/src/test/java/com/integration/test/IntegrationTests.java
+++ integration-test/src/test/java/com/integration/test/IntegrationTests.java
@@ -1,30 +1,15 @@
 package com.integration.test;
 
-import com.integration.test.repository.ProductRepository;
-import com.shopcommon.model.Product;
-import com.sun.xml.internal.bind.v2.TODO;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-@RunWith(SpringJUnit4ClassRunner.class)
 @IntegrationTest("server.port:0")
 @SpringApplicationConfiguration(classes = {IntegrationTestApplication.class})
 public class IntegrationTests {
