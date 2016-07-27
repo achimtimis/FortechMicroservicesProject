91520f81e82de618f8c7b8b0968f04c20b871722
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-27 16:39:53 +0300

    Product-Service integration tests reimplemented to better practice

--- product-service/src/main/java/com/productservice/service/ProductService.java
+++ product-service/src/main/java/com/productservice/service/ProductService.java
@@ -24,7 +24,6 @@ public class ProductService {
     }
 
     public Product create(Product product){
-        System.out.println(productRepository.save(product));
         return productRepository.save(product);
     }
 
