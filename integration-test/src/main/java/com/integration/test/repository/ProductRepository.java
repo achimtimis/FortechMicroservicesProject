5850155d1597d3b914feaaed6f3cd1b06eabcde3
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-27 11:29:30 +0300

    Product-Service integration tests implemented in integration-test module

--- integration-test/src/main/java/com/integration/test/repository/ProductRepository.java
+++ integration-test/src/main/java/com/integration/test/repository/ProductRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by Flaviu Cicio on 06.07.2016.
  */
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
 }
