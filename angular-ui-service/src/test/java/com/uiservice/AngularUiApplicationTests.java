4d639b7ff80e0973cafcbf0cfcb4c6f0fe3c3836
Flaviu Cicio <cicioflaviu@gmail.com>
2016-07-28 10:20:42 +0300

    Product-Service integration tests polish

--- angular-ui-service/src/test/java/com/uiservice/AngularUiApplicationTests.java
+++ angular-ui-service/src/test/java/com/uiservice/AngularUiApplicationTests.java
@@ -20,48 +20,52 @@ import static org.mockito.Mockito.when;
 @SpringApplicationConfiguration(classes = AngularUiApplication.class)
 public class AngularUiApplicationTests {
 
-	@Mock
-	private ProductController productController;
-
-	@Autowired
-	RabbitTemplate rabbitTemplate;
-
-//	@Test
-//	public void contextLoads() {
+//	@Mock
+//	private ProductController productController;
 //
-//		when(productController.getProducts()).thenReturn(sendProductsToQueue()).;
+//	@Autowired
+//	RabbitTemplate rabbitTemplate;
 //
+////	@Test
+////	public void contextLoads() {
+////
+////		when(productController.getProducts()).thenReturn(sendProductsToQueue()).;
+////
+////	}
+//
+//	private List<Product> sendProductsToQueue(){
+//		List<Product> products = new ArrayList<>();
+//		Product p1 = new Product();
+//		p1.setPrice(10);
+//		p1.setStock(100);
+//		p1.setName("product1");
+//		p1.setId(1L);
+//
+//
+//		Product p2 = new Product();
+//		p2.setPrice(20);
+//		p2.setStock(200);
+//		p2.setName("product2");
+//		p2.setId(2L);
+//
+//
+//		Product p3 = new Product();
+//		p3.setPrice(30);
+//		p3.setStock(300);
+//		p3.setName("product3");
+//		p3.setId(3L);
+//
+//		products.add(p1);
+//		products.add(p2);
+//		products.add(p3);
+//
+//		rabbitTemplate.convertAndSend("product-queue", products);
+//
+//		return products;
 //	}
 
-	private List<Product> sendProductsToQueue(){
-		List<Product> products = new ArrayList<>();
-		Product p1 = new Product();
-		p1.setPrice(10);
-		p1.setStock(100);
-		p1.setName("product1");
-		p1.setId(1L);
+	@Test
+	public void load(){
 
-
-		Product p2 = new Product();
-		p2.setPrice(20);
-		p2.setStock(200);
-		p2.setName("product2");
-		p2.setId(2L);
-
-
-		Product p3 = new Product();
-		p3.setPrice(30);
-		p3.setStock(300);
-		p3.setName("product3");
-		p3.setId(3L);
-
-		products.add(p1);
-		products.add(p2);
-		products.add(p3);
-
-		rabbitTemplate.convertAndSend("product-queue", products);
-
-		return products;
 	}
-
 }
