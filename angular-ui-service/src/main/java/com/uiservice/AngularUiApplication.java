f2c0a60950406ae11737924b051a3eed20c78a0a
Flaviu Cicio <cicioflaviu@gmail.com>
2016-08-03 11:26:50 +0300

    Implemented message listening architecture between angular-ui-service and product-service

--- angular-ui-service/src/main/java/com/uiservice/AngularUiApplication.java
+++ angular-ui-service/src/main/java/com/uiservice/AngularUiApplication.java
@@ -1,5 +1,6 @@
 package com.uiservice;
 
+import com.uiservice.messaging.ProductProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,17 +9,19 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerA
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 
 @EnableCircuitBreaker
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class})
 @EnableEurekaClient
+@EnableBinding(ProductProcessor.class)
 public class AngularUiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AngularUiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AngularUiApplication.class, args);
+    }
 }
--- angular-ui-service/src/main/java/com/uiservice/controller/ProductUiController.java
+++ angular-ui-service/src/main/java/com/uiservice/controller/ProductUiController.java
@@ -2,21 +2,22 @@ package com.uiservice.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.shopcommon.model.Product;
+import com.uiservice.messaging.ProductProcessor;
 import org.apache.log4j.Logger;
-import org.apache.log4j.Priority;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.*;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,9 +27,24 @@ import java.util.List;
 @RequestMapping(value = "/api/products")
 public class ProductUiController {
 
+    /*******
+     * LISTENERS
+     *******/
+    private List<Product> listenAllProducts;
+    private Product listenProduct;
+
+    /*******
+     * CHANNELS
+     *******/
+    @Autowired
+    @Qualifier(ProductProcessor.OUTPUT_DELETE)
+    private MessageChannel channel_delete;
+    @Autowired
+    @Qualifier(ProductProcessor.OUTPUT_CREATE)
+    private MessageChannel channel_create;
+
     Logger logger = Logger.getLogger(ProductUiController.class);
 
-    RestTemplate restTemplate = new RestTemplate();
 
     @Autowired
     RabbitTemplate rabbitTemplate;
@@ -39,25 +55,15 @@ public class ProductUiController {
     @Autowired
     private LoadBalancerClient loadBalancer;
 
-    private List<Product> getAllProductsFallback(){
-        logger.info("getAllProductsFallback");
-        rabbitAdmin.purgeQueue("product-queue", true);
-        return new ArrayList<Product>();
-    }
-
     /**
      * returns all the products in the database
      *
      * @return
      */
-    @HystrixCommand(fallbackMethod = "getAllProductsFallback")
     @RequestMapping(method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "getAllProductsFallback")
     public List<Product> getAllProducts() {
-
-        List<Product> products = new ArrayList<>();
-
         try {
-
             ServiceInstance instance = loadBalancer.choose("product-service");
             URI uri = instance.getUri();
             URL obj = new URL(uri.toString() + "/products");
@@ -65,83 +71,20 @@ public class ProductUiController {
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.getResponseCode();
 
-            products.addAll((List<Product>) rabbitTemplate.receiveAndConvert("product-queue"));
-
-            logger.info("Received from product-queue: " + products);
-
-        } catch (MalformedURLException e) {
-            logger.error(e.getMessage());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
-        return products;
+        return this.listenAllProducts;
     }
 
     /**
-     * adds/updates the product to the database
-     * @param product
-     * @return
-     */
-    private Product addProductFallback(Product product) {
-        logger.info("addProductFallback");
-        return null;
-    }
-
-    @HystrixCommand(fallbackMethod = "addProductFallback")
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Product addProduct(@RequestBody Product product) {
-
-        try {
-//            rabbitAdmin.purgeQueue("product-queue", false);
-
-            logger.info("Product " + product + " was send to product-queue");
-
-            rabbitTemplate.convertAndSend("product-queue", product);
-
-            ServiceInstance instance = loadBalancer.choose("product-service");
-            URI uri = instance.getUri();
-            uri = new URI(uri.toString() + "/products");
-
-            product = restTemplate.postForObject(uri, product, Product.class);
-        } catch (URISyntaxException e) {
-            logger.error(e.getMessage());
-        }
-        return product;
-    }
-
-    /**
-     * removes the product given by its id from the database
+     * Returns a specific product from product-service
      *
      * @param id
+     * @return
      */
-    private void removeProductFallback(Long id) {
-        logger.info("removeProductFallback");
-    }
-
-    @HystrixCommand(fallbackMethod = "removeProductFallback")
-    @RequestMapping(method = RequestMethod.DELETE)
-    public void removeProduct(@RequestParam("id") Long id) {
-        try {
-            ServiceInstance instance = loadBalancer.choose("product-service");
-            URI uri = instance.getUri();
-
-            URL obj = new URL(uri.toString() + "/products/" + id);
-
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("DELETE");
-            con.getResponseCode();
-
-            logger.info("Deleted product with id " + id + " from repository");
-        } catch (MalformedURLException e) {
-            logger.error(e.getMessage());
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Product getProduct(@PathVariable("id") Long id) {
-        Product product = null;
         try {
             ServiceInstance instance = loadBalancer.choose("product-service");
             URI uri = instance.getUri();
@@ -151,18 +94,91 @@ public class ProductUiController {
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
             con.getResponseCode();
-
-            product = (Product)rabbitTemplate.receiveAndConvert("product-queue");
-
-            logger.info("Received from product-queue: " + product);
-
-
-        } catch (MalformedURLException e) {
-            logger.error(e.getMessage());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+        return listenProduct;
+    }
+
+    @HystrixCommand(fallbackMethod = "addProductFallback")
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Product addProduct(@RequestBody Product product) {
+
+        logger.info("SENT VIA OUTPUT_CREATE: " + product);
+        channel_create.send(MessageBuilder.withPayload(product).build());
+
         return product;
+    }
+
+
+    @HystrixCommand(fallbackMethod = "removeProductFallback")
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void removeProduct(@RequestParam("id") Long id) {
+
+        logger.info("SENT VIA OUTPUT_DELETE: " + id);
+        this.channel_delete.send(MessageBuilder.withPayload(id).build());
+    }
+
+
+    /**************************************/
+    /**********LISTENERS*******************/
+    /**************************************/
+
+    /**
+     * Receiver for all products
+     *
+     * @param products
+     */
+    @StreamListener(ProductProcessor.INPUT_GETALL)
+    public void listenerGetAllProducts(List<Product> products) {
+        logger.info("RECEIVED FROM INPUT_GETALL: " + products);
+        listenAllProducts = products;
+    }
+
+    /**
+     * Receiver for product
+     *
+     * @param product
+     */
+    @StreamListener(ProductProcessor.INPUT_GET)
+    public void listenerGetProduct(Product product) {
+        logger.info("RECEIVED FROM INPUT_GET: " + product);
+        listenProduct = product;
+    }
+
+
+    /**************************************/
+    /**********FALLBACK METHODS************/
+    /**************************************/
+
+    /**
+     * Fallback method for getAllProducts
+     *
+     * @return
+     */
+    public List<Product> getAllProductsFallback(Throwable e) {
+        logger.info("getAllProductsFallback");
+        return listenAllProducts;
+    }
+
+    /**
+     * Fallback method for addProduct
+     *
+     * @param product
+     * @return
+     */
+    Product addProductFallback(Product product) {
+        logger.info("addProductFallback");
+        return null;
+    }
+
+    /**
+     * Fallback method for delete
+     *
+     * @param id
+     */
+    void removeProductFallback(Long id) {
+        logger.info("removeProductFallback");
     }
 
 }
